677bb172f07e2e4b0e68a58a3bca215607446f3c
David Lang <David.lang398@gmail.com>
2022-12-10 10:07:35 +0100

    feat: auth functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,6 +6,8 @@ import {
 	TextField,
 	Typography
 } from '@mui/material';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import useField from '../hooks/useField';
 import {
@@ -16,6 +18,7 @@ import {
 import { signIn, signUp } from '../utils/firebase';
 
 const Login = () => {
+	const navigate = useNavigate();
 	const [email, emailProps] = useField(
 		'email',
 		requiredValidator,
@@ -26,6 +29,16 @@ const Login = () => {
 		requiredValidator,
 		passwordValidator
 	);
+
+	const onSignUp = useCallback(async () => {
+		await signUp(email, password);
+		navigate('/');
+	}, [email, password]);
+
+	const onSignIn = useCallback(async () => {
+		await signIn(email, password);
+		navigate('/');
+	}, [email, password]);
 	return (
 		<Container
 			maxWidth="sm"
@@ -59,14 +72,10 @@ const Login = () => {
 						mt: 2
 					}}
 				>
-					<Button
-						variant="outlined"
-						color="secondary"
-						onClick={() => signUp(email, password)}
-					>
+					<Button variant="outlined" color="secondary" onClick={onSignUp}>
 						Sign Up
 					</Button>
-					<Button variant="contained" onClick={() => signIn(email, password)}>
+					<Button variant="contained" onClick={onSignIn}>
 						Sign In
 					</Button>
 				</Box>
