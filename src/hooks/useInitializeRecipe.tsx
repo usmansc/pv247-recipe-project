8708d238d6c7f9bad2bc9e8780bf8e4465a544a2
David Lang <David.lang398@gmail.com>
2022-12-13 20:11:51 +0100

    refactor: use hook useCallback

--- src/hooks/useInitializeRecipe.tsx
+++ src/hooks/useInitializeRecipe.tsx
@@ -1,84 +1,29 @@
-import { query, where, getDocs, updateDoc, addDoc } from 'firebase/firestore';
-import { useState } from 'react';
+import { addDoc, getDocs, query, updateDoc, where } from 'firebase/firestore';
+import { useCallback, useState } from 'react';
 
 import {
-	Recipe,
-	Tag,
 	Amount,
-	Ingredient,
 	amountsCollection,
+	Ingredient,
 	ingredientsCollection,
+	Recipe,
 	recipesCollection,
+	Tag,
 	tagsCollection
 } from '../utils/firebase';
 
 import useLoggedInUser from './useLoggedInUser';
 
+const genUniqueId = () => {
+	const dateStr = Date.now().toString(36);
+	const randomStr = Math.random().toString(36).substring(2, 8);
+	return `${dateStr}-${randomStr}`;
+};
+
 const useInitializeRecipe = (recipeProp: Recipe) => {
-	const addTagToRecipe = () => {
-		const newTags = [...recipe.tags, tag];
-		setRecipe({ ...recipe, tags: newTags });
-		setTag({ id: genUniqueId(), name: '' });
-	};
-
-	const addIngredientToRecipe = (name: string, calories: number) => {
-		ingredient.name = name;
-		ingredient.amount.unit = unit;
-		ingredient.amount = amount;
-		ingredient.energy = calories;
-		const newIngredients = [...recipe.ingredients, ingredient];
-		setRecipe({ ...recipe, ingredients: newIngredients });
-		setIngredientId(genUniqueId());
-		setUnit('Grams');
-		setAmount({
-			id: genUniqueId(),
-			ingredient: ingredientId,
-			value: 0,
-			unit
-		});
-		setIngredient({
-			id: ingredientId,
-			recipe: recipe.id,
-			name: '',
-			amount,
-			energy: 0
-		});
-	};
-
-	const updateRecipe = async () => {
-		const q = query(recipesCollection, where('id', '==', recipe.id));
-		const querySnapshot = await getDocs(q);
-		querySnapshot.forEach(doc => {
-			updateDoc(doc.ref, recipe);
-		});
-	};
-
-	const createRecipe = () => {
-		addDoc(recipesCollection, recipe);
-		recipe.ingredients.forEach(ingredient => {
-			addDoc(ingredientsCollection, ingredient);
-		});
-
-		recipe.tags.forEach(tag => {
-			addDoc(tagsCollection, tag);
-		});
-
-		recipe.ingredients.forEach(ingredient => {
-			addDoc(amountsCollection, ingredient.amount);
-		});
-	};
-
-	const addRecipe = () => {
+	const addRecipe = useCallback(() => {
 		recipeProp ? updateRecipe() : createRecipe();
-	};
-
-	const genUniqueId = () => {
-		const dateStr = Date.now().toString(36);
-
-		const randomStr = Math.random().toString(36).substring(2, 8);
-
-		return `${dateStr}-${randomStr}`;
-	};
+	}, [recipeProp]);
 
 	const [ingredientId, setIngredientId] = useState<string>(genUniqueId());
 
@@ -112,52 +57,136 @@ const useInitializeRecipe = (recipeProp: Recipe) => {
 		energy: 0
 	});
 
-	const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setAmount({ ...amount, value: parseInt(event.target.value) });
-	};
-
-	const handleUnitChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setUnit(event.target.value);
-	};
-
-	const handleTagClick = (tag: string) => {
-		const newTags = recipe.tags.filter(t => t.id !== tag);
+	const addTagToRecipe = useCallback(() => {
+		const newTags = [...recipe.tags, tag];
 		setRecipe({ ...recipe, tags: newTags });
-	};
+		setTag({ id: genUniqueId(), name: '' });
+	}, [setRecipe, setTag, tag, recipe]);
 
-	const handleIngredientClick = (ingredient: string) => {
-		console.log(ingredient);
-		const newIngredients = recipe.ingredients.filter(i => i.id !== ingredient);
-		setRecipe({ ...recipe, ingredients: newIngredients });
-	};
+	const addIngredientToRecipe = useCallback(
+		(name: string, calories: number) => {
+			ingredient.name = name;
+			ingredient.amount.unit = unit;
+			ingredient.amount = amount;
+			ingredient.energy = calories;
+			const newIngredients = [...recipe.ingredients, ingredient];
+			setRecipe({ ...recipe, ingredients: newIngredients });
+			setIngredientId(genUniqueId());
+			setUnit('Grams');
+			setAmount({
+				id: genUniqueId(),
+				ingredient: ingredientId,
+				value: 0,
+				unit
+			});
+			setIngredient({
+				id: ingredientId,
+				recipe: recipe.id,
+				name: '',
+				amount,
+				energy: 0
+			});
+		},
+		[recipe, ingredient, ingredientId, unit, amount]
+	);
 
-	const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setRecipe({ ...recipe, name: event.target.value });
-	};
+	const updateRecipe = useCallback(async () => {
+		const q = query(recipesCollection, where('id', '==', recipe.id));
+		const querySnapshot = await getDocs(q);
+		querySnapshot.forEach(doc => {
+			updateDoc(doc.ref, recipe);
+		});
+	}, [recipe]);
 
-	const handleIngredientChange = (
-		event: React.ChangeEvent<HTMLInputElement>
-	) => {
-		setIngredient({ ...ingredient, name: event.target.value });
-	};
+	const createRecipe = useCallback(async () => {
+		await Promise.all([
+			addDoc(recipesCollection, recipe),
+			...recipe.ingredients.map(ingredient =>
+				addDoc(ingredientsCollection, ingredient)
+			),
+			...recipe.tags.map(tag => addDoc(tagsCollection, tag)),
+			...recipe.ingredients.map(ingredient =>
+				addDoc(amountsCollection, ingredient.amount)
+			)
+		]);
+	}, [recipe]);
 
-	const handleTagChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setTag({ ...tag, name: event.target.value });
-	};
+	const handleAmountChange = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			const parsed = parseInt(event.target.value);
+			setAmount(prev => ({ ...prev, value: isNaN(parsed) ? 0 : parsed }));
+		},
+		[amount, setAmount]
+	);
 
-	const handleDescriptionChange = (
-		event: React.ChangeEvent<HTMLInputElement>
-	) => {
-		setRecipe({ ...recipe, description: event.target.value });
-	};
+	const handleUnitChange = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			setUnit(event.target.value);
+		},
+		[setUnit]
+	);
 
-	const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setRecipe({ ...recipe, image: event.target.value });
-	};
+	const handleTagClick = useCallback(
+		(tag: string) => {
+			setRecipe(prev => ({
+				...prev,
+				tags: prev.tags.filter(t => t.id !== tag)
+			}));
+		},
+		[recipe, setRecipe]
+	);
 
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-	};
+	const handleIngredientClick = useCallback(
+		(ingredient: string) => {
+			setRecipe(prev => ({
+				...prev,
+				ingredients: prev.ingredients.filter(i => i.id !== ingredient)
+			}));
+		},
+		[recipe, setRecipe]
+	);
+
+	const handleNameChange = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			setRecipe(prev => ({ ...prev, name: event.target.value }));
+		},
+		[setRecipe]
+	);
+
+	const handleIngredientChange = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			setIngredient(prev => ({ ...prev, name: event.target.value }));
+		},
+		[setIngredient]
+	);
+
+	const handleTagChange = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			setTag(prev => ({ ...prev, name: event.target.value }));
+		},
+		[setTag]
+	);
+
+	const handleDescriptionChange = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			setRecipe(prev => ({ ...prev, description: event.target.value }));
+		},
+		[setRecipe]
+	);
+
+	const handleImageChange = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			setRecipe(prev => ({ ...prev, image: event.target.value }));
+		},
+		[setRecipe]
+	);
+
+	const handleSubmit = useCallback(
+		(event: React.FormEvent<HTMLFormElement>) => {
+			event.preventDefault();
+		},
+		[]
+	);
 
 	return {
 		tag,
